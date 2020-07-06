12d3b9267e5c6ada9ba5ea7b3ece270a3d62f97d
Filipe Santos <filipesantos@wearepixelmatters.com>
2020-07-06 15:27:20 +0100

    fix: identation error

--- src/store/movies/reducers.ts
+++ src/store/movies/reducers.ts
@@ -16,25 +16,25 @@ export const moviesReducer = (
   action: MovieActionTypes
 ): IMoviesState => {
   switch (action.type) {
-    case REQUEST_LATEST_START:
-      return {
-        isFetching: true,
-        latest: undefined,
-        error: undefined,
-      };
-    case REQUEST_LATEST_SUCCESS:
-      return {
-        isFetching: false,
-        latest: action.latest,
-        error: undefined,
-      };
-    case REQUEST_LATEST_ERROR:
-      return {
-        isFetching: false,
-        latest: undefined,
-        error: action.error,
-      };
-    default:
-      return state;
+  case REQUEST_LATEST_START:
+    return {
+      isFetching: true,
+      latest: undefined,
+      error: undefined,
+    };
+  case REQUEST_LATEST_SUCCESS:
+    return {
+      isFetching: false,
+      latest: action.latest,
+      error: undefined,
+    };
+  case REQUEST_LATEST_ERROR:
+    return {
+      isFetching: false,
+      latest: undefined,
+      error: action.error,
+    };
+  default:
+    return state;
   }
 };
