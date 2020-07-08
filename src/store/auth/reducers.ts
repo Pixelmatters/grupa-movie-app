375a1187883c5e4fe53cc2985a2719d14e0389b7
Filipe Santos <filipesantos@wearepixelmatters.com>
2020-07-08 17:51:30 +0100

    chore: lint all files

--- src/store/auth/reducers.ts
+++ src/store/auth/reducers.ts
@@ -28,68 +28,68 @@ export const authReducer = (
   action: AuthActionTypes
 ): IRequestAuthState => {
   switch (action.type) {
-  case REQUEST_AUTH_TOKEN_START:
-    return {
-      ...state,
-      isFetchingRequestToken: true,
-      failedRequestToken: false,
-      requestToken: undefined,
-    };
-  case REQUEST_AUTH_TOKEN_SUCCESS:
-    return {
-      ...state,
-      isFetchingRequestToken: false,
-      failedRequestToken: false,
-      requestToken: action.requestToken,
-    };
-  case REQUEST_AUTH_TOKEN_ERROR:
-    return {
-      ...state,
-      isFetchingRequestToken: false,
-      failedRequestToken: true,
-      requestToken: undefined,
-    };
-  case REQUEST_SESSION_ID_START:
-    return {
-      ...state,
-      isFetchingCreateSession: true,
-      failedCreateSession: false,
-      sessionId: undefined,
-    };
-  case REQUEST_SESSION_ID_SUCCESS:
-    return {
-      ...state,
-      isFetchingCreateSession: false,
-      failedCreateSession: false,
-      sessionId: action.createSession.session_id,
-    };
-  case REQUEST_SESSION_ID_ERROR:
-    return {
-      ...state,
-      isFetchingCreateSession: false,
-      failedCreateSession: true,
-      sessionId: undefined,
-    };
-  case REQUEST_DELETE_SESSION_START:
-    return {
-      ...state,
-      isFetchingDeleteSession: true,
-      failedDeleteSession: false,
-    };
-  case REQUEST_DELETE_SESSION_SUCCESS:
-    return {
-      ...state,
-      isFetchingDeleteSession: false,
-      failedDeleteSession: false,
-      sessionId: undefined,
-    };
-  case REQUEST_DELETE_SESSION_ERROR:
-    return {
-      ...state,
-      isFetchingDeleteSession: false,
-      failedDeleteSession: true,
-    };
-  default:
-    return state;
+    case REQUEST_AUTH_TOKEN_START:
+      return {
+        ...state,
+        isFetchingRequestToken: true,
+        failedRequestToken: false,
+        requestToken: undefined,
+      };
+    case REQUEST_AUTH_TOKEN_SUCCESS:
+      return {
+        ...state,
+        isFetchingRequestToken: false,
+        failedRequestToken: false,
+        requestToken: action.requestToken,
+      };
+    case REQUEST_AUTH_TOKEN_ERROR:
+      return {
+        ...state,
+        isFetchingRequestToken: false,
+        failedRequestToken: true,
+        requestToken: undefined,
+      };
+    case REQUEST_SESSION_ID_START:
+      return {
+        ...state,
+        isFetchingCreateSession: true,
+        failedCreateSession: false,
+        sessionId: undefined,
+      };
+    case REQUEST_SESSION_ID_SUCCESS:
+      return {
+        ...state,
+        isFetchingCreateSession: false,
+        failedCreateSession: false,
+        sessionId: action.createSession.session_id,
+      };
+    case REQUEST_SESSION_ID_ERROR:
+      return {
+        ...state,
+        isFetchingCreateSession: false,
+        failedCreateSession: true,
+        sessionId: undefined,
+      };
+    case REQUEST_DELETE_SESSION_START:
+      return {
+        ...state,
+        isFetchingDeleteSession: true,
+        failedDeleteSession: false,
+      };
+    case REQUEST_DELETE_SESSION_SUCCESS:
+      return {
+        ...state,
+        isFetchingDeleteSession: false,
+        failedDeleteSession: false,
+        sessionId: undefined,
+      };
+    case REQUEST_DELETE_SESSION_ERROR:
+      return {
+        ...state,
+        isFetchingDeleteSession: false,
+        failedDeleteSession: true,
+      };
+    default:
+      return state;
   }
 };
--- src/store/movie/reducers.ts
+++ src/store/movie/reducers.ts
@@ -28,70 +28,70 @@ export const movieReducer = (
   action: MovieActionTypes
 ): IMovieState => {
   switch (action.type) {
-  case REQUEST_MOVIE_START:
-    return {
-      ...state,
-      isFetchingMovie: true,
-      failedFetchingMovie: false,
-      movie: undefined,
-    };
-  case REQUEST_MOVIE_SUCCESS:
-    return {
-      ...state,
-      isFetchingMovie: false,
-      failedFetchingMovie: false,
-      movie: action.movie,
-    };
-  case REQUEST_MOVIE_ERROR:
-    return {
-      ...state,
-      isFetchingMovie: false,
-      failedFetchingMovie: true,
-      movie: undefined,
-    };
-  case REQUEST_POPULAR_START:
-    return {
-      ...state,
-      isFetchingPopular: true,
-      failedFetchingPopular: false,
-      popular: undefined,
-    };
-  case REQUEST_POPULAR_SUCCESS:
-    return {
-      ...state,
-      isFetchingPopular: false,
-      failedFetchingPopular: false,
-      popular: action.popular,
-    };
-  case REQUEST_POPULAR_ERROR:
-    return {
-      ...state,
-      isFetchingPopular: false,
-      failedFetchingPopular: true,
-      popular: undefined,
-    };
-  case REQUEST_LATEST_START:
-    return {
-      ...state,
-      isFetchingLatest: true,
-      failedFetchingLatest: false,
-      latest: undefined,
-    };
-  case REQUEST_LATEST_SUCCESS:
-    return {
-      ...state,
-      isFetchingLatest: false,
-      failedFetchingLatest: false,
-      latest: action.latest,
-    };
-  case REQUEST_LATEST_ERROR:
-    return {
-      ...state,
-      isFetchingLatest: false,
-      failedFetchingLatest: true,
-      latest: undefined,
-    };
-  default:
-    return state;
+    case REQUEST_MOVIE_START:
+      return {
+        ...state,
+        isFetchingMovie: true,
+        failedFetchingMovie: false,
+        movie: undefined,
+      };
+    case REQUEST_MOVIE_SUCCESS:
+      return {
+        ...state,
+        isFetchingMovie: false,
+        failedFetchingMovie: false,
+        movie: action.movie,
+      };
+    case REQUEST_MOVIE_ERROR:
+      return {
+        ...state,
+        isFetchingMovie: false,
+        failedFetchingMovie: true,
+        movie: undefined,
+      };
+    case REQUEST_POPULAR_START:
+      return {
+        ...state,
+        isFetchingPopular: true,
+        failedFetchingPopular: false,
+        popular: undefined,
+      };
+    case REQUEST_POPULAR_SUCCESS:
+      return {
+        ...state,
+        isFetchingPopular: false,
+        failedFetchingPopular: false,
+        popular: action.popular,
+      };
+    case REQUEST_POPULAR_ERROR:
+      return {
+        ...state,
+        isFetchingPopular: false,
+        failedFetchingPopular: true,
+        popular: undefined,
+      };
+    case REQUEST_LATEST_START:
+      return {
+        ...state,
+        isFetchingLatest: true,
+        failedFetchingLatest: false,
+        latest: undefined,
+      };
+    case REQUEST_LATEST_SUCCESS:
+      return {
+        ...state,
+        isFetchingLatest: false,
+        failedFetchingLatest: false,
+        latest: action.latest,
+      };
+    case REQUEST_LATEST_ERROR:
+      return {
+        ...state,
+        isFetchingLatest: false,
+        failedFetchingLatest: true,
+        latest: undefined,
+      };
+    default:
+      return state;
   }
 };
