f2efef914333f501a61ecbdcb53253c1ace746aa
Junior D. Oliveira <juniordiasoli@gmail.com>
2020-07-08 11:45:09 +0100

    feat(frontpage,mansonry): added list movies (#36)
    * feat(frontpage,mansonry): added list movies
    
    * request changes

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,30 +1,44 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Grid } from '@material-ui/core';
 import headerBg from '../../assets/images/header-bg.jpg';
 import Header from '../Header/Header';
-import Footer from '../Footer/Footer';
 import './Home.css';
+import MovieList from '../MovieList/MovieList';
+import { useDispatch } from 'react-redux';
+import { fetchPopular } from '../../store/movie/thunks';
 
 const useStyles = makeStyles(styles => ({
   root: {
     flexGrow: 1,
     fontFamily: styles.typography.fontFamily,
+    width: '100%'
   },
-  backgroundTop: {
-    backgroundImage: `url(${headerBg})`
+  header: {
+    backgroundImage: `url(${headerBg})`,
+    width: '100%',
+    height: '10rem'
   },
   mainContainer: {
     marginTop: '2rem'
   },
 }));
 
-function App() {
+function Home() {
   const classes = useStyles();
+  const dispatch = useDispatch();
+
+  useEffect(()=> {
+    dispatch(fetchPopular());
+
+  }, [dispatch]);
+
   return (
     <div className={classes.root}>
-      <Grid item xs={12} className={classes.backgroundTop}>
-        <Header />
+      <Grid xs={12} item >
+        <Grid className={classes.header}>
+          <Header />
+        </Grid>
 
         <Grid item xs={12} sm={12} className={classes.mainContainer}>
           <main>
@@ -34,15 +48,14 @@ function App() {
               </div>
             </Grid>
             <Grid item xs={12} sm={12}>
-              Content
+              <MovieList />
             </Grid>
           </main>
         </Grid>
       </Grid>
 
-      <Footer />
     </div>
   );
 }
 
-export default App;
+export default Home;
