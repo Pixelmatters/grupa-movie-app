4c801f7d44a114a3bea6472ca4ecc0df2a674873
Filipe Santos <34577702+FilipeAVS@users.noreply.github.com>
2020-07-08 12:17:00 +0100

    Authentication Flow (#39)
    * feat(auth store): add auth store
    
    * feat(auth): implement auth flow and create the approved component
    
    * fix(Approved): removed warning
    
    * feat: initial authentication flow
    
    * feat(authentication): login and logout flow. store session id on local storage
    
    * fix: removed unnecessary code
    
    * chore: add redirect url to env
    
    Co-authored-by: Tiago Pereira <tiagopereira@wearepixelmatters.com>

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,13 +3,15 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 
 const Home = lazy(() => import('../Home/Home'));
+const Approved = lazy(() => import('../Approved/Approved'));
 
 function App() {
   return (
     <Router>
       <Suspense fallback={<div>Loading...</div>}>
         <Switch>
-          <Route exact path="/" component={Home}/>
+          <Route exact path="/" component={Home} />
+          <Route path="/approved/" component={Approved} />
         </Switch>
       </Suspense>
     </Router>
