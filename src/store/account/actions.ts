b895c4a2d73a3f904932bc2b09ab1ab0ffdf1c14
Tiago Pereira <tiagopereira@wearepixelmatters.com>
2020-07-08 18:33:43 +0100

    fix(account store): fix some typos

--- src/store/account/actions.ts
+++ src/store/account/actions.ts
@@ -7,48 +7,48 @@ import {
   REQUEST_MOVIE_WATCHLIST_SUCCESS,
   REQUEST_RATED_MOVIES_ERROR,
   REQUEST_RATED_MOVIES_START,
-  REQUEST_RATED_MOVIES_SUCCESSS,
-  IAccount
+  REQUEST_RATED_MOVIES_SUCCESS,
+  IAccount,
 } from './types';
 import { IMovie } from '../../api/models';
 
 export const requestAccountDetailsStart = (): any => ({
-  type: REQUEST_ACCOUNT_DETAILS_START
+  type: REQUEST_ACCOUNT_DETAILS_START,
 });
 
 export const requestAccountDetailsSuccess = (account: IAccount): any => ({
   type: REQUEST_ACCOUNT_DETAILS_SUCCESS,
-  account: account
+  account: account,
 });
 
 export const requestAccountDetailsError = (): any => ({
-  type: REQUEST_ACCOUNT_DETAILS_ERROR
+  type: REQUEST_ACCOUNT_DETAILS_ERROR,
 });
 
 export const requestMovieWatchlistStart = (): any => ({
-  type: REQUEST_MOVIE_WATCHLIST_START
+  type: REQUEST_MOVIE_WATCHLIST_START,
 });
 
 export const requestMovieWatchlistSuccess = (
   watchList: Array<IMovie>
 ): any => ({
   type: REQUEST_MOVIE_WATCHLIST_SUCCESS,
-  watchList: watchList
+  watchList: watchList,
 });
 
 export const requestMovieWatchlistError = (): any => ({
-  type: REQUEST_MOVIE_WATCHLIST_ERROR
+  type: REQUEST_MOVIE_WATCHLIST_ERROR,
 });
 
-export const requestRateMoviesStart = (): any => ({
-  type: REQUEST_RATED_MOVIES_START
+export const requestRatedMoviesStart = (): any => ({
+  type: REQUEST_RATED_MOVIES_START,
 });
 
-export const requestRateMoviesSuccess = (ratedMovies: Array<IMovie>): any => ({
-  type: REQUEST_RATED_MOVIES_SUCCESSS,
-  ratedMovies: ratedMovies
+export const requestRatedMoviesSuccess = (ratedMovies: Array<IMovie>): any => ({
+  type: REQUEST_RATED_MOVIES_SUCCESS,
+  ratedMovies: ratedMovies,
 });
 
-export const requestRateMoviesError = (): any => ({
-  type: REQUEST_RATED_MOVIES_ERROR
+export const requestRatedMoviesError = (): any => ({
+  type: REQUEST_RATED_MOVIES_ERROR,
 });
