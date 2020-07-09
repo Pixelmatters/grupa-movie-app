3f2a238433a6f554b06dd170777bbb2598159803
Junior D. Oliveira <juniordiasoli@gmail.com>
2020-07-09 10:13:56 +0100

    Feat(frontpage, improvements): Card responsivity - Card Options - Card Overlay improvement (#62)
    * Some ui fixes
    
    * fix(ui): change the homepage iteration to click
    
    * feat(card options): added options to cards
    
    * requested changes

--- src/components/MovieList/MovieList.tsx
+++ src/components/MovieList/MovieList.tsx
@@ -10,6 +10,8 @@ import {
 } from '@material-ui/core';
 import { IMovie } from '../../api/models';
 import Masonry from 'react-masonry-css';
+import { PlaylistAdd, UnfoldMore } from '@material-ui/icons';
+import { getImageURL, getNotFoundImage } from '../../api/api';
 
 const useStyles = makeStyles((theme: Theme) => ({
   container: {
@@ -48,21 +50,70 @@ const useStyles = makeStyles((theme: Theme) => ({
     position: 'relative',
     cursor: 'pointer',
     marginBottom: '1rem',
-    '&:hover': {
-      '& > div': {
-        height: '17.5rem',
+  },
+  checkHelper: {
+    display: 'none',
+    '&:checked': {
+      '& ~ div': {
+        height: '99%',
         transition: 'height 1s ease',
         padding: '0.5rem',
         transform: 'translateZ(0)',
       },
-      '& > img': {
-        filter: 'grayscale(80%)',
+      '& ~ div > $movieRate': {
+        opacity: 1,
       },
+      '& ~ img': {
+        filter: 'grayscale(80%)',
+      },     
     },
   },
-
+  movieSinopse: {
+    height: '15rem',
+    [theme.breakpoints.down('sm')]:{
+      height: '10rem',
+    }
+  },
   itemTitle: {
     fontSize: '1rem',
+  },
+  movieImage: {
+    animation: '$fadeIn ease 3s',
+    transform: 'translateZ(0)',
+    width: 400,
+    height: 600,
+    [theme.breakpoints.down('sm')]:{
+      width: 350,
+      height: 500,
+    }
+  },
+  loader: {
+    display: 'flex',
+    justifyContent: 'center',
+  },
+  cardOptions: {
+    display: 'flex',
+    marginTop: '8rem',
+    padding: '2rem',
+    justifyContent: 'space-between',
+    fontWeight: 800,
+    color: '#F9BE51',
+    '& > div': {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+  },
+  movieRate: {
+    opacity: 0,
+    width: '2rem',
+    height: '2rem',
+    backgroundColor: '#d88e06',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: '50%',
+    float: 'right',
   },
   '@keyframes fadeIn': {
     '0%': {
@@ -71,14 +122,6 @@ const useStyles = makeStyles((theme: Theme) => ({
     '100%': {
       opacity: 1,
     },
-  },
-  movieImage: {
-    animation: '$fadeIn ease 5s',
-  },
-
-  loader: {
-    display: 'flex',
-    justifyContent: 'center',
   },
 }));
 
@@ -107,29 +150,46 @@ const MovieList: FunctionComponent = () => {
     }
   }, [moreRecentList]);
 
+  const renderImage = (path?: string, altText?: string) => {
+    const localPath = path 
+      ? getImageURL(path) 
+      : getNotFoundImage('400x600/FFFFFF');
+      
+    return (            
+      <img 
+        className={classes.movieImage}
+        src={localPath} 
+        alt={altText} />);
+  };
+
   return (
     <Grid container className={classes.container}>
       <Masonry
         breakpointCols={breakpointColumnsObj}
         className={classes.masonryGrid}
-        columnClassName={classes.masonryGridColumn}
-      >
-        {movieList &&
-          movieList.map((item: any) => (
-            <Box key={item.id} className={classes.itemWrapper}>
-              <img
-                className={classes.movieImage}
-                height="600"
-                src={`https://image.tmdb.org/t/p/w500${item.poster_path}`}
-                alt={item.title}
-              />
+        columnClassName={classes.masonryGridColumn}>
+        {movieList && movieList.map((item: IMovie) => (
+          <label key={item.id}>
+            <Box  className={classes.itemWrapper}>
+              <input type="radio" name="radio" value="small" className={classes.checkHelper} />
+              {renderImage(item.poster_path, item.title)}
               <Box className={classes.info}>
-                <Box component="h5" className={classes.itemTitle}>
-                  {item.title}
-                </Box>
-                <Box component="span">{item.overview}</Box>
+                <Box className={classes.movieRate}>{item.vote_average}</Box>
+                <Box component="h5" className={classes.itemTitle}>{item.title}</Box>
+                  <Box component="p" className={classes.movieSinopse}>
+                    {item.overview}
+                  </Box>
+                  <Box className={classes.cardOptions}>
+                    <Box>  
+                      <UnfoldMore /> See more
+                    </Box>
+                    <Box>                    
+                      <PlaylistAdd /> Watch list
+                    </Box>
+                  </Box>
               </Box>
             </Box>
+          </label>
           ))}
       </Masonry>
       <Grid item xs={12} className={classes.loader}>
