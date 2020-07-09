a052c41ab996e8b984f3c9c224b6567779f2bba0
Filipe Santos <34577702+FilipeAVS@users.noreply.github.com>
2020-07-09 12:03:20 +0100

    fix(api): default params where overriding additional params (#64)

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,22 +1,13 @@
-import Axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
+import Axios, { AxiosResponse } from 'axios';
 import { IAddToWatchlist } from './models';
 
 const BASE_URL: string = 'https://api.themoviedb.org/3';
 const API_KEY: string | undefined = process.env.REACT_APP_API_KEY;
 
-const defaultConfig: AxiosRequestConfig = {
-  params: {
-    api_key: API_KEY
-  },
-  headers: {
-    'Content-Type': 'application/json'
-  }
-};
-
 enum Method {
   GET = 'GET',
   POST = 'POST',
-  DELETE = 'DELETE'
+  DELETE = 'DELETE',
 }
 
 const joinBaseUrlWithRoute = (route: string) => `${BASE_URL}/${route}`;
@@ -28,7 +19,13 @@ const exec = (
   data?: any
 ): Promise<AxiosResponse<any | undefined>> => {
   const url = joinBaseUrlWithRoute(route);
-  const config = { params: params, data: data, ...defaultConfig };
+  const config = {
+    params: { api_key: API_KEY, ...params },
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: data,
+  };
   if (method === Method.GET) {
     return Axios.get(url, config);
   } else if (method === Method.POST) {
@@ -50,18 +47,18 @@ export const createRequestToken = () =>
   exec('authentication/token/new', Method.GET);
 export const createSession = (requestToken: string) =>
   exec('authentication/session/new', Method.POST, null, {
-    request_token: requestToken
+    request_token: requestToken,
   });
 export const deleteSession = (sessionId: string) =>
   exec('authentication/session', Method.DELETE, null, {
-    session_id: sessionId
+    session_id: sessionId,
   });
 
 // Account
 
 export const getAccountDetails = (sessionId: string) =>
   exec(`account?session_id=${sessionId}`, Method.GET, {
-    session_id: sessionId
+    session_id: sessionId,
   });
 
 export const getRatedMovies = (accountId: number) =>
@@ -82,5 +79,7 @@ export const addToWatchlist = (
     data
   );
 
-  export const getImageURL = (suffix: string) => `https://image.tmdb.org/t/p/w500${suffix}`;
-  export const getNotFoundImage = (settings: string) => `https://via.placeholder.com/${settings}?text=No%20Image`;
+export const getImageURL = (suffix: string) =>
+  `https://image.tmdb.org/t/p/w500${suffix}`;
+export const getNotFoundImage = (settings: string) =>
+  `https://via.placeholder.com/${settings}?text=No%20Image`;
